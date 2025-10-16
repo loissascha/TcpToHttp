dd4076128cd085ba2eecc28a0ba9e786e90b9bae
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-16 14:58:42 +0200

    read request

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,44 +1,43 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net"
+	"tcpToHttp/internal/request"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	out := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(out)
-		str := ""
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				out <- str
-				str = ""
-			}
-
-			str += string(data)
-
-		}
-
-		if len(str) != 0 {
-			out <- str
-		}
-	}()
-
-	return out
-}
+// func getLinesChannel(f io.ReadCloser) <-chan string {
+// 	out := make(chan string, 1)
+//
+// 	go func() {
+// 		defer f.Close()
+// 		defer close(out)
+// 		str := ""
+// 		for {
+// 			data := make([]byte, 8)
+// 			n, err := f.Read(data)
+// 			if err != nil {
+// 				break
+// 			}
+// 			data = data[:n]
+// 			if i := bytes.IndexByte(data, '\n'); i != -1 {
+// 				str += string(data[:i])
+// 				data = data[i+1:]
+// 				out <- str
+// 				str = ""
+// 			}
+//
+// 			str += string(data)
+//
+// 		}
+//
+// 		if len(str) != 0 {
+// 			out <- str
+// 		}
+// 	}()
+//
+// 	return out
+// }
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
@@ -51,9 +50,14 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read :%s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			panic(err)
 		}
+		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n", r.RequestLine.Method, r.RequestLine.RequestTarget, r.RequestLine.HttpVersion)
+		// for line := range getLinesChannel(conn) {
+		// 	fmt.Printf("read :%s\n", line)
+		// }
 	}
 
 }
