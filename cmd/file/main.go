7eaa9516a750a13ff434d8edbd44245d25975bfc
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-16 10:18:56 +0200

    read with channel

--- cmd/file/main.go
+++ cmd/file/main.go
@@ -3,36 +3,51 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
+
 func main() {
-	file, err := os.Open("messages.txt")
+	f, err := os.Open("messages.txt")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := file.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-
-		str += string(data)
-
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
-	}
 }
