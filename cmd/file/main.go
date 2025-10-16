f0dbdef8099492900c7f2401f81e2718d82e2019
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-16 10:08:39 +0200

    read from file

--- cmd/file/main.go
+++ cmd/file/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	file, err := os.Open("messages.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	for {
+		data := make([]byte, 8)
+		n, err := file.Read(data)
+		if err != nil {
+			break
+		}
+		fmt.Printf("read: %s\n", string(data[:n]))
+	}
 }
