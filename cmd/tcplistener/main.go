8615052db2685f3f2a063a41db7f70d12ef3f835
Sascha Loishandl <sascha.loishandl@gmail.com>
2025-10-16 14:59:04 +0200

    up

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -6,39 +6,6 @@ import (
 	"tcpToHttp/internal/request"
 )
 
-// func getLinesChannel(f io.ReadCloser) <-chan string {
-// 	out := make(chan string, 1)
-//
-// 	go func() {
-// 		defer f.Close()
-// 		defer close(out)
-// 		str := ""
-// 		for {
-// 			data := make([]byte, 8)
-// 			n, err := f.Read(data)
-// 			if err != nil {
-// 				break
-// 			}
-// 			data = data[:n]
-// 			if i := bytes.IndexByte(data, '\n'); i != -1 {
-// 				str += string(data[:i])
-// 				data = data[i+1:]
-// 				out <- str
-// 				str = ""
-// 			}
-//
-// 			str += string(data)
-//
-// 		}
-//
-// 		if len(str) != 0 {
-// 			out <- str
-// 		}
-// 	}()
-//
-// 	return out
-// }
-
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
@@ -55,9 +22,6 @@ func main() {
 			panic(err)
 		}
 		fmt.Printf("Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n", r.RequestLine.Method, r.RequestLine.RequestTarget, r.RequestLine.HttpVersion)
-		// for line := range getLinesChannel(conn) {
-		// 	fmt.Printf("read :%s\n", line)
-		// }
 	}
 
 }
